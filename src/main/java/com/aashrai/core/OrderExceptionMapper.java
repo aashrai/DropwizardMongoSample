9b3c172813ae4c542651ff22731d0bd36c62b5dd
Aashrai <aashrai@gozefo.com>
2019-03-26 22:00:25 +0530

    Add logging to exception mapper

--- src/main/java/com/aashrai/core/OrderExceptionMapper.java
+++ src/main/java/com/aashrai/core/OrderExceptionMapper.java
@@ -1,18 +1,23 @@
 package com.aashrai.core;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
+@Slf4j
 public class OrderExceptionMapper implements ExceptionMapper<Exception> {
 
     @Override
     public Response toResponse(Exception exception) {
         if (exception instanceof OrderException) {
             OrderException orderException = (OrderException) exception;
+            log.info(orderException.getMessage());
             return Response.status(orderException.getStatus())
                     .entity(new OrderExceptionInfo(orderException.getStatus(), orderException.getMessage()))
                     .build();
         }
+        log.error("Unexpected error in order API", exception);
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
     }
 }
